edb9677a2bf9910638430c4c99639f1ddff6f620
Ken Lenhart <omnipaid@gmail.com>
2022-11-13 13:16:33 -0800

    Finish tests for youtube service

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -1,9 +1,20 @@
-class YoutubeService 
-  def self.conn 
-    Faraday.new('https://www.googleapis.com')
+class YoutubeService
+  def self.country_search_endpoint(country)
+    response = conn.get('youtube/v3/search', q: country)
+    parse(response)
   end
 
-  def self.parse(api_key)
+  private
+
+  def self.conn 
+    Faraday.new(url: 'https://www.googleapis.com') do |f|
+      f.params['channelId'] = 'UCluQ5yInbeAkkeCndNnUhpw'
+      f.params['part'] = 'snippet'
+      f.params['key'] = ENV['youtube_api_key']
+    end
+  end
+
+  def self.parse(api_data)
     JSON.parse(api_data.body, symbolize_names: true)
   end
 end
--- spec/services/youtube_service_spec.rb
+++ spec/services/youtube_service_spec.rb
@@ -7,8 +7,26 @@ RSpec.describe YoutubeService, type: :service do
     end
 
     it 'has successful response on connection' do
-      response = YoutubeService.conn.get()
+      response = YoutubeService.conn.get('youtube/v3/search')
+      
       expect(response.status).to eq(200)
+    end
+
+    it 'has search by country endpoint' do
+      response = YoutubeService.country_search_endpoint('Australia')
+      
+      expect(response).to be_a(Hash)
+      expect(response).to have_key(:items)
+      expect(response[:items]).to be_a(Array)
+      response[:items].each do |item|
+        expect(item).to be_a(Hash)
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(Hash)
+        expect(item).to have_key(:snippet)
+        expect(item[:snippet]).to be_a(Hash)
+        expect(item[:snippet]).to have_key(:title)
+        expect(item[:snippet][:title]).to be_a(String)
+      end
     end
   end
 end
