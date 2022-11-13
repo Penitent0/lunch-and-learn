0011395a5bcbd7124fd886e079fac2f4c7fc0376
Ken Lenhart <omnipaid@gmail.com>
2022-11-12 16:24:27 -0800

    Refactor edaman recipe poro instantiation

--- app/facades/edaman_facade.rb
+++ app/facades/edaman_facade.rb
@@ -1,7 +1,8 @@
 class EdamanFacade 
   def self.recipe_search(country)
     EdamanService.recipe_search_endpoint(country)[:hits].map do |recipe|
-      EdamanRecipe.new(recipe[:recipe], country)
+      recipe_hash = recipe[:recipe]
+      EdamanRecipe.new(recipe_hash[:url], recipe_hash[:label], recipe_hash[:image], country)
     end
   end
 end
