dac00c5fa1d00a9b562a7df1bb13b4e2ce957e30
Ken Lenhart <omnipaid@gmail.com>
2022-11-13 22:21:37 -0800

    Refactor verify country to application controller, refactor tests to match

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -5,16 +5,4 @@ class Api::V1::RecipesController < ApplicationController
     recipes = EdamamFacade.recipe_search(params[:country])
     render json: RecipeSerializer.recipes(recipes)
   end
-
-  private
-
-  def verify_country
-    if params[:country].nil? || !RestCountriesFacade.all_countries.include?(params[:country].capitalize)
-      render json: ErrorSerializer.bad_country, status: 400
-    end 
-  end
-
-  def country_picker
-    RestCountriesFacade.all_countries.sample
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def verify_country
+    if params[:country].nil? || !RestCountriesFacade.all_countries.include?(params[:country].capitalize)
+      render json: ErrorSerializer.bad_country, status: 400
+    end 
+  end
 end
