a64d0b4fa3c3df7c5e48ad2473bab0a3123642f1
Ken Lenhart <omnipaid@gmail.com>
2022-11-14 08:47:29 -0800

    Add tourist_sight_info poro

--- app/facades/geoapify_facade.rb
+++ app/facades/geoapify_facade.rb
@@ -1,7 +1,7 @@
 class GeoapifyFacade 
-  def self.tourist_info(lat, long)
+  def self.tourist_info(lat, lng)
     GeoapifyService.tourist_info_endpoint(lat, lng)[:features].map do |feature|
-      TouristSightInfo.new(feature)
+      TouristSightInfo.new(feature[:properties][:name], feature[:properties][:formatted], feature[:properties][:place_id])
     end
   end
 end
