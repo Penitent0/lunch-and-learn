1df99f800ea2310cef4884846d1bb09586536fee
Ken Lenhart <omnipaid@gmail.com>
2022-11-13 18:07:15 -0800

    Add users_controller.rb and routes for user create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/recipes', to: 'recipes#index'
       get '/learning_resources', to: 'learning_resources#index'
+      post '/users', to: 'users#create'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
