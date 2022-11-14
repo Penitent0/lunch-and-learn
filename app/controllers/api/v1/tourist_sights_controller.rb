eb4140a7b387e3d77b5e7eac44fd6df697476f5e
Ken Lenhart <omnipaid@gmail.com>
2022-11-14 09:56:30 -0800

    Add sad path tests for bad country requests

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TouristSightsController < ApplicationController
-  before_action :verify_country, only: %i[:index]
-  
+  before_action :verify_country
+
   def index
     capital_info = RestCountriesFacade.one_country_lat_lng(params[:country])
     sights = GeoapifyFacade.tourist_info(capital_info.lat, capital_info.lng)
--- spec/requests/tourist_sights/tourist_sights_spec.rb
+++ spec/requests/tourist_sights/tourist_sights_spec.rb
@@ -2,11 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'tourist_sights' do
   describe 'GET /tourist_sights', vcr: { record: :new_episodes } do
-    it 'has successful http status and correct json structure' do
+    it 'has successful http status' do
       get "/api/v1/tourist_sights", params: { country: "Mexico" }
 
       expect(response).to be_successful
-    
+
+      get "/api/v1/tourist_sights", params: { country: "Latvia" }
+
+      expect(response).to be_successful
+
+      get "/api/v1/tourist_sights", params: { country: "France" }
+
+      expect(response).to be_successful
+    end
+
+    it 'has correst json response structure' do 
+      get "/api/v1/tourist_sights", params: { country: "Mexico" }
+
       sights = JSON.parse(response.body, symbolize_names: true)
 
       expect(sights).to be_a(Hash)
@@ -21,6 +33,20 @@ RSpec.describe 'tourist_sights' do
         expect(sight[:attributes][:address]).to be_a(String)
         expect(sight[:attributes][:place_id]).to be_a(String)
       end
+    end
+
+    it 'has sad path if country params does not match REST countries list' do
+      get "/api/v1/tourist_sights", params: { country: "Made Up Country" }
+
+      expect(response).to have_http_status(:bad_request)
+
+      get "/api/v1/tourist_sights", params: { country: "1234567890" }
+
+      expect(response).to have_http_status(:bad_request)
+
+      get "/api/v1/tourist_sights", params: { country: "Franc" }
+
+      expect(response).to have_http_status(:bad_request) 
     end
   end
 end
