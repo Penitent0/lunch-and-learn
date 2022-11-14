ff718b8d9cad02d9f9167671ef7f291ac83c56e4
Ken Lenhart <omnipaid@gmail.com>
2022-11-14 09:49:31 -0800

    Add before action verify country to tourist sights controller

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,4 +1,6 @@
-class Api::V1::TouristSightsController < ApplicationController 
+class Api::V1::TouristSightsController < ApplicationController
+  before_action :verify_country, only: %i[:index]
+  
   def index
     capital_info = RestCountriesFacade.one_country_lat_lng(params[:country])
     sights = GeoapifyFacade.tourist_info(capital_info.lat, capital_info.lng)
