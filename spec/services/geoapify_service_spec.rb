a461ff62de0f589f506afa6e06691999a0e188d8
Ken Lenhart <omnipaid@gmail.com>
2022-11-14 08:37:36 -0800

    Complete geoapify service tests

--- spec/services/geoapify_service_spec.rb
+++ spec/services/geoapify_service_spec.rb
@@ -6,9 +6,21 @@ RSpec.describe GeoapifyService, type: :service do
       expect(GeoapifyService.new).to be_a(GeoapifyService)
     end
 
-    it 'has tourist info endpoint' do
-      response = GeoapifyService.tourist_info_endpoint(47.6062, 122.3321)
+    it 'has tourist info endpoint for given lat/lng' do
+      response = GeoapifyService.tourist_info_endpoint(47.620422,-122.335167)
       
+      expect(response).to be_a(Hash)
+      expect(response).to have_key(:features)
+      expect(response[:features]).to be_a(Array)
+      response[:features].each do |feature|
+        expect(feature).to be_a(Hash)
+        expect(feature).to have_key(:properties)
+        expect(feature[:properties]).to be_a(Hash)
+        expect(feature[:properties]).to have_key(:name)
+        expect(feature[:properties][:name]).to be_a(String)
+        expect(feature[:properties][:formatted]).to be_a(String)
+        expect(feature[:properties][:place_id]).to be_a(String)
+      end
     end
   end
 end
