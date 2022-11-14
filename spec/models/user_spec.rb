7c7545d5b80656e6d835fa5bfd16003c5e5bfe85
Ken Lenhart <omnipaid@gmail.com>
2022-11-13 22:34:05 -0800

    Add password_digest to user model, added test for secure password to model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should have_secure_password }
   end
 end
