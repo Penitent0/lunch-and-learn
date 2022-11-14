aab0bf3bc470040ce26e228ab63858ff3147278d
Ken Lenhart <omnipaid@gmail.com>
2022-11-13 17:30:07 -0800

    Finish Iteration 2

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::LearningResourcesController < ApplicationController
   def index
     video = YoutubeFacade.country_search(params[:country]).first
-    require 'pry'; binding.pry
     images = UnsplashFacade.country_search(params[:country])
 
     render json: MediaSerializer.media(params[:country], video, images)
--- spec/requests/learning_resources_spec.rb
+++ spec/requests/learning_resources_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "LearningResources", type: :request do
-  describe "GET /learning_resources" do
+  describe "GET /learning_resources", vcr: { record: :new_episodes } do
     it "returns http success" do
-      get "/learning_resources/"
+      get "/api/v1/learning_resources"
       expect(response).to have_http_status(:success)
     end
 
@@ -13,7 +13,43 @@ RSpec.describe "LearningResources", type: :request do
 
       learning_resource = JSON.parse(response.body, symbolize_names: true)
 
-      require 'pry'; binding.pry
+      expect(learning_resource).to be_a(Hash)
+      expect(learning_resource).to have_key(:data)
+      expect(learning_resource[:data]).to be_a(Hash)
+      expect(learning_resource[:data]).to have_key(:id)
+      expect(learning_resource[:data][:id]).to eq("null")
+      expect(learning_resource[:data]).to have_key(:attributes)
+      expect(learning_resource[:data][:attributes]).to be_a(Hash)
+      expect(learning_resource[:data][:attributes][:country]).to eq("Germany")
+      expect(learning_resource[:data][:attributes]).to have_key(:video)
+      expect(learning_resource[:data][:attributes][:video]).to be_a(Hash)
+      expect(learning_resource[:data][:attributes][:video][:title]).to be_a(String)
+      expect(learning_resource[:data][:attributes][:video][:youtube_video_id]).to be_a(String)
+      expect(learning_resource[:data][:attributes][:images]).to be_a(Array)
+      learning_resource[:data][:attributes][:images].each do |image|
+        expect(image[:alt_tag]).to be_a(String)
+        expect(image[:url]).to be_a(String)
+      end
+    end
+
+    it 'returns empty arrays for video and images if no images or videos are found' do 
+      get "/api/v1/learning_resources", params: { country: "planedogcatbirdbuildingshovel" }
+      expect(response).to be_successful
+
+      learning_resource = JSON.parse(response.body, symbolize_names: true)
+
+      expect(learning_resource[:data][:attributes][:video]).to eq([])
+      expect(learning_resource[:data][:attributes][:images]).to eq([])
+      expect(learning_resource[:data][:attributes][:country]).to eq("planedogcatbirdbuildingshovel")
+
+      get "/api/v1/learning_resources", params: { country: "1234567890" }
+      expect(response).to be_successful
+
+      learning_resource = JSON.parse(response.body, symbolize_names: true)
+
+      expect(learning_resource[:data][:attributes][:video]).to eq([])
+      expect(learning_resource[:data][:attributes][:images]).to eq([])
+      expect(learning_resource[:data][:attributes][:country]).to eq("1234567890")
     end
   end
 end
