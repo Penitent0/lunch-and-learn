f55b406c539dc85e3c6333272ddff8d346a0ce8d
Ken Lenhart <omnipaid@gmail.com>
2022-11-14 19:30:23 -0800

    Add tests for favorite creation

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Favorites", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/favorites/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /create", vcr: { record: :new_episodes } do
+    before :each do
+      valid_user = User.create!(name: "Valid", email: "supervalid@validmail.com", password: "test", password_confirmation: "test", api_key: "012345")
+    end
+    
+    it "returns http created on successful favorite creation" do
+      valid_params = {
+        api_key: "012345",
+        country: "Czechia",
+        recipe_link: "https://www.recipes.com".
+        reciple_title: "Best Recipe On The Internet"
+      }
+      expect { post "/api/v1/favorites", params: valid_params }.to change(Favorite, :count).by(+1)
     end
   end
 
