395643baed0652c57beaa5b7bca01f5c6f15656e
Ken Lenhart <omnipaid@gmail.com>
2022-11-14 19:46:07 -0800

    Finish iteration 4

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,24 +2,24 @@ class Api::V1::FavoritesController < ApplicationController
   before_action :verify_key
 
   def create
-    favorite = Favorite.new(favorites_params)
+    user = User.find_by(api_key: params[:api_key])
+    favorite = user.favorites.new(favorites_params)
     if favorite.save 
       render json: { success: "Favorite added successfully" }, status: 201
     else
-      render json: ErrorSerializer.parse_errors(favorite)
+      render json: ErrorSerializer.parse_errors(favorite.errors), status: 400
     end
-    require 'pry'; binding.pry
   end
 
   private 
 
   def verify_key
-    if !User.find_by(params[:api_key]) || !params[:api_key]
+    if !User.find_by(api_key: params[:api_key]) || !params[:api_key]
       render json: ErrorSerializer.bad_key, status: 400
     end
   end
 
   def favorites_params 
-    require(:favorite).permit(:country, :recipe_link, :recipe_title)
+    params.permit(:country, :recipe_link, :recipe_title)
   end
 end
--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -5,16 +5,43 @@ RSpec.describe "Favorites", type: :request do
     before :each do
       valid_user = User.create!(name: "Valid", email: "supervalid@validmail.com", password: "test", password_confirmation: "test", api_key: "012345")
     end
-    
+
     it "returns http created on successful favorite creation" do
       valid_params = {
         api_key: "012345",
         country: "Czechia",
-        recipe_link: "https://www.recipes.com".
-        reciple_title: "Best Recipe On The Internet"
+        recipe_link: "https://www.recipes.com",
+        recipe_title: "Best Recipe On The Internet"
       }
+
       expect { post "/api/v1/favorites", params: valid_params }.to change(Favorite, :count).by(+1)
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'has sad path if user api_key is not found' do
+      invalid_params = {
+        api_key: "543210",
+        country: "Czechia",
+        recipe_link: "https://www.recipes.com",
+        recipe_title: "Best Recipe On The Internet"
+      }
+
+      post "/api/v1/favorites", params: invalid_params
+
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it 'has sad path if favorites required params are not includedin request' do 
+      invalid_params = {
+        api_key: "012345",
+        country: "Czechia",
+        recipe_link: "https://www.recipes.com"
+      }
+
+      post "/api/v1/favorites", params: invalid_params
+
+      expect(response).to have_http_status(:bad_request)
     end
   end
-
 end
